8c32ab939c30577aa31b60f0e11e842e09f887c0
Kishor <developer.kishor@gmail.com>
2018-08-16 21:57:47 +0530

    Initial

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-var DATASET_ID = 'ciwm29xyd00082tmocm5l6osb';
-var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/' + DATASET_ID + '/';
-var DATASETS_ACCESS_TOKEN = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNpaG9mOGljdTBibmN0aGo3NWR6Y3Q0aXQifQ.X73YugnJDlhZEhxz2X86WA';
-var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
-var STYLESHEET = 'mapbox://styles/planemad/cih4qzr0w0012awltzvpie7qa';
+var DATASET_ID = 'cjkwig8wk0wom31o7hym3tv5n';
+var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/kishorv06/' + DATASET_ID + '/';
+var DATASETS_ACCESS_TOKEN = 'sk.eyJ1Ijoia2lzaG9ydjA2IiwiYSI6ImNqa3dpaTQxNjBkcm0zd213YXIxMnJjM3QifQ.Hh_Ed-RMVXuQ8GL1ub_3lQ';
+var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1Ijoia2lzaG9ydjA2IiwiYSI6ImNqa3dpYzR2djB6YmEzdnFuNXlzcnloMDEifQ.bpeBUY001h2018QHS30QzQ';
+var STYLESHEET = 'mapbox://styles/kishorv06/cjkwidat21qo02ruj4t2fcmxy';
 var LOAD_INFO_LAYERS = ['chennai-relief-camps', 'chennai-relief-camps-22nov'];
